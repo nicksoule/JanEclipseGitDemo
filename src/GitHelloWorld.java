aea99d4c3bf442b8606afd01d25b82157033752c
nicksoule <nicksoule@live.com>
2018-01-23 13:39:31 -0500

    first commit helloworld

---
+++ src/GitHelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * Nicholas Soule
+ * 
+ * This is a simple hello world program that will be used to push to github
+ */
+public class GitHelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
